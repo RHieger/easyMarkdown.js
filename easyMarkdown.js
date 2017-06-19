1c4763a3388184126937ac1962786eeeae685f4a
Robert <rhieger@nyc.rr.com>
2017-06-19 13:36:29 -0400

    Skeleton Alpha easyMarkdown.js Library Begun

---
+++ easyMarkdown.js
@@ -0,0 +1,16 @@
+// easyMarkdown.js
+//
+// Robert Hieger
+//
+// v0.0.1a
+
+// This is the very first alpha version of easyMarkdown, and
+// thus, very experimental.
+//
+// I will begin by creating a JSON data source containing a
+// translation table for all Markdown specifiers to equivalent
+// HTML5 elements.
+//
+// My hope is to be able to compare the source Markdown document
+// line against the JSON dictionary and then append the equivalent
+// HTML5 element to the DOM.
